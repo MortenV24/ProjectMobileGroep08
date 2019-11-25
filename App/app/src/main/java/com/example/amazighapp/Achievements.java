c6c7578f2961a1c6fa20d36a1c0c0b83e6ee9202
Morten <85415@mborijnland.nl>
2019-11-25 11:19:51 +0100

    Juiste classnames aangepast

--- App/app/src/main/java/com/example/amazighapp/Achievements.java
+++ App/app/src/main/java/com/example/amazighapp/Achievements.java
@@ -4,11 +4,11 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
 
-public class oefen extends AppCompatActivity {
+public class Achievements extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_oefen);
+        setContentView(R.layout.activity_achievements);
     }
 }
