fd87e0e6708ceabc4660eca9aee8d879085b0d7a
Yaimo Collins <1040905@mborijnland.nl>
2019-12-02 13:41:55 +0100

    firebase goeie implementations gemaakt

--- App/app/src/main/java/com/example/amazighapp/Speel.java
+++ App/app/src/main/java/com/example/amazighapp/Speel.java
@@ -3,12 +3,43 @@ package com.example.amazighapp;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.widget.Toast;
+
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.ValueEventListener;
 
 public class Speel extends AppCompatActivity {
+
+    private DatabaseReference databaseSpeel;
+    private ValueEventListener onChangeListener;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_speel);
+
+        databaseSpeel = FirebaseDatabase.getInstance().getReference().child("categorieën");
+    }
+
+
+    public void onStart() {
+        super.onStart();
+        ValueEventListener onChangeListener = new ValueEventListener() {
+            @Override
+            public void onDataChange(DataSnapshot dataSnapshot) {
+                Toast.makeText(Speel.this, "Er is nieuwe data",
+                        Toast.LENGTH_SHORT).show();
+            }
+
+            @Override
+            public void onCancelled(DatabaseError databaseError) {
+                Toast.makeText(Speel.this, "Failed to load data.",
+                        Toast.LENGTH_SHORT).show();
+            }
+        };
+        databaseSpeel.addValueEventListener(onChangeListener);
     }
 }
